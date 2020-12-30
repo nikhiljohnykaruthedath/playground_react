8dbf70117d3e694dcab6885f0d19a8d2920f50a2
njohnyk <69707376+njohnyk@users.noreply.github.com>
2020-12-30 10:21:57 -0800

    Adding reviews and accoridion components

---
+++ accordion/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import questions from './data';
+import SingleQuestion from './Question';
+function App() {
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and Answer about Login</h3>
+        <section className="info">
+          {
+            questions.map((question) => {
+              return <SingleQuestion key={question.id} {...question} />
+            })
+          }
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
